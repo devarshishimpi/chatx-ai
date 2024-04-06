8f36bd1cea933231606aeb057d12eedf9170ce30
Devarshi Shimpi <devarshishimpi@gmail.com>
2024-04-06 19:55:55 +0530

    add:menu bar section

--- src/panel.js
+++ src/panel.js
@@ -6,3 +6,25 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.classList.toggle("hidden");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownToggle = document.querySelector(
+    ".relative.inline-block.ml-auto button",
+  );
+  const dropdownMenu = document.querySelector(
+    ".relative.inline-block.ml-auto .absolute",
+  );
+
+  dropdownToggle.addEventListener("click", function () {
+    dropdownMenu.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (
+      !dropdownToggle.contains(event.target) &&
+      !dropdownMenu.contains(event.target)
+    ) {
+      dropdownMenu.classList.add("hidden");
+    }
+  });
+});
